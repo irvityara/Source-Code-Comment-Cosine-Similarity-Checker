6435d6ebe95324e1d9a39b66bd11c16b4198a42e
Ipikk <irvityara456@gmail.com>
2025-07-16 16:31:09 +0700

    Initial commit

--- api/index.py
+++ api/index.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
 model = SentenceTransformer('all-MiniLM-L6-v2')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/similarity', methods=['POST'])
 def similarity():
@@ -22,6 +18,3 @@ def similarity():
     score = cosine_similarity([embeddings[0]], [embeddings[1]])[0][0]
 
     return jsonify({'similarity': float(round(score, 4))})
-
-if __name__ == '__main__':
-    app.run(debug=True)
